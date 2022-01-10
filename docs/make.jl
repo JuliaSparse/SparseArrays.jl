8b7ca0269aaf4d8a0c26f0576cba6367f8cbf8e2
Dilum Aluthge <dilum@aluthge.com>
2022-01-10 02:15:08 -0500

    [CI] Build the docs with `strict = [:doctest]`, and fix the broken doctests (#5)

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,16 @@
-using Documenter, SparseArrays
+using SparseArrays
+using Documenter: DocMeta, makedocs, deploydocs
+
+DocMeta.setdocmeta!(SparseArrays, :DocTestSetup, :(using SparseArrays; using LinearAlgebra); recursive=true)
 
 makedocs(
     modules = [SparseArrays],
     sitename = "SparseArrays",
     pages = Any[
         "SparseArrays" => "index.md"
-        ]
+        ];
+    # strict = true,
+    strict = Symbol[:doctest],
     )
 
 deploydocs(repo = "github.com/JuliaLang/SparseArrays.jl.git")
