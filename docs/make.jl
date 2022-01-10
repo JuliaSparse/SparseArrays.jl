9051a2e956b9da29fc929689ac476e5682bcdbab
Dilum Aluthge <dilum@aluthge.com>
2022-01-10 00:25:46 -0500

    Revert "Try building the docs with `strict = true`"
    This reverts commit c8477c8a01d5b3258ab588b962bbbd5013cb1a60.

--- docs/make.jl
+++ docs/make.jl
@@ -5,8 +5,7 @@ makedocs(
     sitename = "SparseArrays",
     pages = Any[
         "SparseArrays" => "index.md"
-        ];
-    strict = true,
+        ]
     )
 
 deploydocs(repo = "github.com/JuliaLang/SparseArrays.jl.git")
