c8477c8a01d5b3258ab588b962bbbd5013cb1a60
Dilum Aluthge <dilum@aluthge.com>
2022-01-10 00:23:47 -0500

    Try building the docs with `strict = true`

--- docs/make.jl
+++ docs/make.jl
@@ -5,7 +5,8 @@ makedocs(
     sitename = "SparseArrays",
     pages = Any[
         "SparseArrays" => "index.md"
-        ]
+        ];
+    strict = true,
     )
 
 deploydocs(repo = "github.com/JuliaLang/SparseArrays.jl.git")
