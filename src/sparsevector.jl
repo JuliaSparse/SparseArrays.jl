d31042721b256d3e2c080d85e4d15afa1c936c36
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-06-21 20:48:12 -0400

    Fix typo
    i missed a trailing `.

--- src/sparsevector.jl
+++ src/sparsevector.jl
@@ -19,7 +19,7 @@ a *sorted* vector of non-zero indices, and a vector of non-zero values.
 For instance, the Vector `[5, 6, 0, 7]` can be represented as 
 ```julia
 SparseVector(4, [1, 2, 4], [5, 6, 7])
-````
+```
 This indicates that the index 1 is 5, the index 2 is 6, the index 3 is `zero(Int)`, and index 4 is 7.
 
 It may be more convenient to create sparse vectors directly from dense vectors using `sparse` as 
