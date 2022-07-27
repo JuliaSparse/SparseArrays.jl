0ad95d02393eb775dc6ad527c6c70b2c5781ad02
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-07-27 14:54:32 -0400

    fix alloc_tests

--- test/higherorderfns.jl
+++ test/higherorderfns.jl
@@ -22,7 +22,7 @@ function test_map_and_map!(A, alloc_tests)
     fX = copy(fA); X = similar(A)
     map!(sin, X, A); X = similar(A) # warmup for @allocated
     map!(sin, X, A); X = similar(A) # warmup for @allocated
-    @test alloc_tests || @allocated(map!(sin, X, A)) == 0
+    @test @allocated(map!(sin, X, A)) == 0 || alloc_tests
     @test map!(sin, X, A) == sparse(map!(sin, fX, fA))
     @test map!(cos, X, A) == sparse(map!(cos, fX, fA))
     @test_throws DimensionMismatch map!(sin, X, spzeros((shapeA .- 1)...))
