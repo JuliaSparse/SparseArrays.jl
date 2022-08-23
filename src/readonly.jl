5e58b0a892d8855d0d5091dc904d62b3e40a5c41
Sobhan Mohammadpour <sobhan.mohammadpour@umontreal.ca>
2022-08-23 05:43:25 -0400

    fix readonly performance problem

--- src/readonly.jl
+++ src/readonly.jl
@@ -10,22 +10,30 @@ For instance resizing a vector to its original length will not throw an error as
 struct ReadOnly{T,V<:AbstractVector{T}} <: AbstractVector{T}
     parent::V
 end
+# ReadOnly of ReadOnly is meaningless
+ReadOnly(x::ReadOnly) = x
 Base.getproperty(::ReadOnly, ::Symbol) = error("Use parent instead.")
 @inline Base.parent(x::ReadOnly) = getfield(x, :parent)
-Base.@propagate_inbounds @inline getindex(x::ReadOnly, v...) = getindex(parent(x), v...)
-for i in [:length, :lastindex]
+
+for i in [:length, :first, :last, :eachindex, :firstindex, :lastindex, :eltype]
     @eval Base.@propagate_inbounds @inline Base.$i(x::ReadOnly) = Base.$i(parent(x))
 end
-Base.@propagate_inbounds @inline Base.setindex!(x::ReadOnly, v, ind...) = if v == getindex(parent(x), ind...)
-    v
-else
-    error("Can't change $(typeof(x)).")
+for i in [:iterate, :axes, :getindex, :similar, :size, :strides]
+    @eval(Base.@propagate_inbounds @inline Base.$i(x::ReadOnly, y...) = Base.$i(parent(x), y...))
 end
-Base.resize!(x::ReadOnly, l) = l == length(parent(x)) ? x : error("can't resize $(typeof(x))")
+Base.unsafe_convert(x::Type{Ptr{T}}, A::ReadOnly) where T = Base.unsafe_convert(x, parent(A))
+Base.elsize(::Type{ReadOnly{T,V}}) where {T,V} = Base.elsize(V)
+Base.@propagate_inbounds @inline Base.setindex!(x::ReadOnly, v, ind...) = if v == getindex(parent(x), ind...)
+        v
+    else
+        error("Can't change $(typeof(x)).")
+    end
+for i in [:IteratorSize, :IndexStyle]
+    @eval(@inline Base.$i(::Type{ReadOnly{T,V}}) where {T,V} = Base.$i(V))
+end
+@inline Base.resize!(x::ReadOnly, l) = l == length(parent(x)) ? x : error("can't resize $(typeof(x))")
 Base.copy(x::ReadOnly) = ReadOnly(copy(parent(x)))
 (==)(x::ReadOnly, y::AbstractVector) = parent(x) == y
 (==)(x::AbstractVector, y::ReadOnly) = x == parent(y)
 (==)(x::ReadOnly, y::ReadOnly) = parent(x) == parent(y)
-size(x::ReadOnly) = size(parent(x))
-size(x::ReadOnly, i) = size(parent(x), i)
 
