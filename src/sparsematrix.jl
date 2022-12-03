996902a49cd5cb21f805d94f4cf188f5793a7447
Fredrik Ekre <ekrefredrik@gmail.com>
2022-12-03 22:36:51 +0100

    doc: add compat annotation for spzeros method for pattern creation. (#310)

--- src/sparsematrix.jl
+++ src/sparsematrix.jl
@@ -2014,6 +2014,9 @@ Create a sparse matrix `S` of dimensions `m x n` with structural zeros at `S[I[k
 
 This method can be used to construct the sparsity pattern of the matrix, and is more
 efficient than using e.g. `sparse(I, J, zeros(length(I)))`.
+
+!!! compat "Julia 1.10"
+    This methods requires Julia version 1.10 or later.
 """
 spzeros(I::AbstractVector, J::AbstractVector) = spzeros(Float64, I, J)
 spzeros(I::AbstractVector, J::AbstractVector, m::Integer, n::Integer) = spzeros(Float64, I, J, m, n)
