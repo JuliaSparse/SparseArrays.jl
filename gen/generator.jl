374821962df53f82427ce04f852d69f2dec1f676
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-04-02 19:14:39 -0400

    Update the gen folder

--- gen/generator.jl
+++ gen/generator.jl
@@ -4,11 +4,13 @@ using Pkg.Artifacts
 using Clang.Generators
 using Clang.Generators.JLLEnvs
 using SuiteSparse_jll
+using JuliaFormatter
 
 cd(@__DIR__)
 
 # headers
 include_dir = joinpath(SuiteSparse_jll.artifact_dir, "include") |> normpath
+
 cholmod_h = joinpath(include_dir, "cholmod.h")
 @assert isfile(cholmod_h)
 
@@ -17,15 +19,6 @@ SuiteSparseQR_C_h = joinpath(include_dir, "SuiteSparseQR_C.h")
 
 umfpack_h = joinpath(include_dir, "umfpack.h")
 @assert isfile(umfpack_h)
-
-amd_h = joinpath(include_dir, "amd.h")
-@assert isfile(amd_h)
-
-colamd_h = joinpath(include_dir, "colamd.h")
-@assert isfile(colamd_h)
-
-ccolamd_h = joinpath(include_dir, "ccolamd.h")
-@assert isfile(ccolamd_h)
 
 # load common option
 options = load_options(joinpath(@__DIR__, "generator.toml"))
@@ -39,9 +32,12 @@ for target in JLLEnvs.JLL_ENV_TRIPLES
     args = get_default_args(target)
     push!(args, "-I$include_dir")
 
-    header_files = [cholmod_h, SuiteSparseQR_C_h, amd_h, colamd_h, ccolamd_h, umfpack_h]
+    header_files = [cholmod_h, SuiteSparseQR_C_h, umfpack_h]
 
     ctx = create_context(header_files, args, options)
 
     build!(ctx)
+
+    path = options["general"]["output_file_path"]
+    format_file(path, YASStyle())
 end
