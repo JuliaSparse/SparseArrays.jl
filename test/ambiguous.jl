ed41a514cb8e5ed1206f1a64a3ba6a53e94fd2f5
Jameson Nash <vtjnash@gmail.com>
2023-08-01 14:58:11 -0400

    test: restore ambiguous test
    Unfortunately, this test has become broken in the interim since it was
    disabled by 176ef646 / a15fe4b7.

--- test/ambiguous.jl
+++ test/ambiguous.jl
@@ -50,30 +50,28 @@ using Test, LinearAlgebra, SparseArrays, Aqua
     end
 end
 
-@testset "detect_ambiguities" begin
-    @test_nowarn detect_ambiguities(SparseArrays; recursive=true, ambiguous_bottom=false)
+let ambig = detect_ambiguities(SparseArrays; recursive=true)
+    @test_broken isempty(ambig)
+    ambig = Set{Any}(((m1.sig, m2.sig) for (m1, m2) in ambig))
+    expect = []
+    push!(expect, (Tuple{typeof(LinearAlgebra.generic_trimatmul!), AbstractVecOrMat, Any, Any, Function, AbstractMatrix, AbstractVecOrMat},
+                   Tuple{typeof(LinearAlgebra.generic_trimatmul!), StridedVecOrMat, Any, Any, Any, Union{Adjoint{var"#s388", var"#s387"}, Transpose{var"#s388", var"#s387"}} where {var"#s388", var"#s387"<:(Union{SparseArrays.AbstractSparseMatrixCSC{Tv, Ti}, SubArray{Tv, 2, <:SparseArrays.AbstractSparseMatrixCSC{Tv, Ti}, Tuple{Base.Slice{Base.OneTo{Int}}, I}} where I<:AbstractUnitRange} where {Tv, Ti})}, AbstractVecOrMat}))
+    push!(expect, (Tuple{typeof(LinearAlgebra.generic_trimatmul!), AbstractVecOrMat, Any, Any, Function, Union{Adjoint{T, S}, Transpose{T, S}} where {T, S}, AbstractVecOrMat},
+                   Tuple{typeof(LinearAlgebra.generic_trimatmul!), StridedVecOrMat, Any, Any, Any, Union{Adjoint{var"#s388", var"#s387"}, Transpose{var"#s388", var"#s387"}} where {var"#s388", var"#s387"<:(Union{SparseArrays.AbstractSparseMatrixCSC{Tv, Ti}, SubArray{Tv, 2, <:SparseArrays.AbstractSparseMatrixCSC{Tv, Ti}, Tuple{Base.Slice{Base.OneTo{Int}}, I}} where I<:AbstractUnitRange} where {Tv, Ti})}, AbstractVecOrMat}))
+    good = true
+    while !isempty(ambig)
+        sigs = pop!(ambig)
+        i = findfirst(==(sigs), expect)
+        if i === nothing
+            println(stderr, "push!(expect, (", sigs[1], ", ", sigs[2], "))")
+            good = false
+            continue
+        end
+        deleteat!(expect, i)
+    end
+    @test isempty(expect)
+    @test good
 end
-
-## This was the older version that was disabled
-
-# let ambig = detect_ambiguities(SparseArrays; recursive=true)
-#     @test isempty(ambig)
-#     ambig = Set{Any}(((m1.sig, m2.sig) for (m1, m2) in ambig))
-#     expect = []
-#     good = true
-#     while !isempty(ambig)
-#         sigs = pop!(ambig)
-#         i = findfirst(==(sigs), expect)
-#         if i === nothing
-#             println(stderr, "push!(expect, (", sigs[1], ", ", sigs[2], "))")
-#             good = false
-#             continue
-#         end
-#         deleteat!(expect, i)
-#     end
-#     @test isempty(expect)
-#     @test good
-# end
 
 ###
 # Now we restore the original env, as promised
