c45b61fe5ca04597df7d61522a4f00c13f25ab0f
Daniel Karrasch <daniel.karrasch@posteo.de>
2023-09-04 14:44:02 +0200

    Code quality cleanup (#438)

--- test/ambiguous.jl
+++ test/ambiguous.jl
@@ -25,13 +25,13 @@ using Test, LinearAlgebra, SparseArrays, Aqua
 
 @testset "code quality" begin
     @testset "Method ambiguity" begin
-        # Aqua.test_ambiguities([SparseArrays, Base, Core])
+        Aqua.test_ambiguities([SparseArrays, Base, Core])
     end
     @testset "Unbound type parameters" begin
         @test_broken Aqua.detect_unbound_args_recursively(SparseArrays) == []
     end
     @testset "Undefined exports" begin
-        Aqua.test_undefined_exports(SparseArrays) == []
+        Aqua.test_undefined_exports(SparseArrays)
     end
     @testset "Compare Project.toml and test/Project.toml" begin
         Aqua.test_project_extras(SparseArrays)
@@ -50,8 +50,23 @@ using Test, LinearAlgebra, SparseArrays, Aqua
     end
 end
 
-@testset "detect_ambiguities" begin
-    @test_nowarn detect_ambiguities(SparseArrays; recursive=true, ambiguous_bottom=false)
+let ambig = detect_ambiguities(SparseArrays; recursive=true)
+    @test isempty(ambig)
+    ambig = Set{Any}(((m1.sig, m2.sig) for (m1, m2) in ambig))
+    expect = []
+    good = true
+    while !isempty(ambig)
+        sigs = pop!(ambig)
+        i = findfirst(==(sigs), expect)
+        if i === nothing
+            println(stderr, "push!(expect, (", sigs[1], ", ", sigs[2], "))")
+            good = false
+            continue
+        end
+        deleteat!(expect, i)
+    end
+    @test isempty(expect)
+    @test good
 end
 
 ## This was the older version that was disabled
