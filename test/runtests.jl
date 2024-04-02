3ce3ff6fe45cab2116d9f7e51cfb7579a2c88eb2
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-04-02 14:49:48 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,6 +24,12 @@ if Base.USE_GPL_LIBS
     # Test multithreaded execution
     @testset "threaded SuiteSparse tests" verbose = true begin
         @testset "threads = $nt" begin
+            include("threads.jl")
+        end
+        # test both nthreads==1 and nthreads>1. spawn a process to test whichever
+        # case we are not running currently.
+        other_nthreads = nt == 1 ? 4 : 1
+        @testset "threads = $other_nthreads" begin
             # 1. If the OS is Windows and we are in GitHub Actions CI, we do NOT run the `threads` tests.
             # 2. If the OS is Windows and we are NOT in GitHub Actions CI, we DO run the `threads` tests.
             # 3. If the OS is NOT Windows, we DO run the `threads` tests.
@@ -34,29 +40,21 @@ if Base.USE_GPL_LIBS
                 @warn "Skipping `threads` tests on Windows on GitHub Actions CI"
                 @test_broken false
             else
-                @info "Beginning `threads` tests..."
-                include("threads.jl")
-                @info "Finished `threads` tests"
-            end
-        end
-        # test both nthreads==1 and nthreads>1. spawn a process to test whichever
-        # case we are not running currently.
-        other_nthreads = nt == 1 ? 4 : 1
-        @testset "threads = $other_nthreads" begin
-            let p, cmd = `$(Base.julia_cmd()) --depwarn=error --startup-file=no threads.jl`
-                p = run(
-                        pipeline(
-                            setenv(
-                                cmd,
-                                "JULIA_NUM_THREADS" => other_nthreads,
-                                dir=@__DIR__()),
-                            stdout = stdout,
-                            stderr = stderr),
-                        wait = false)
-                if !success(p)
-                    error("SuiteSparse threads test failed with nthreads == $other_nthreads")
-                else
-                    @test true # mimic the one @test in threads.jl
+                let p, cmd = `$(Base.julia_cmd()) --depwarn=error --startup-file=no threads.jl`
+                    p = run(
+                            pipeline(
+                                setenv(
+                                    cmd,
+                                    "JULIA_NUM_THREADS" => other_nthreads,
+                                    dir=@__DIR__()),
+                                stdout = stdout,
+                                stderr = stderr),
+                            wait = false)
+                    if !success(p)
+                        error("SuiteSparse threads test failed with nthreads == $other_nthreads")
+                    else
+                        @test true # mimic the one @test in threads.jl
+                    end
                 end
             end
         end
