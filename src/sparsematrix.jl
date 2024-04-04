8dd830082fa807c74a2c9412e1f8e648282ea6fe
Jishnu Bhattacharya <jishnub.github@gmail.com>
2024-04-04 08:24:39 +0530

    SparseMatrixCSC constructor with a Tuple of Integers (#523)
    * SparseMatrixCSC constructor with a Tuple of Integers
    
    * SparseVector constructors
    
    ---------
    
    Co-authored-by: Viral B. Shah <ViralBShah@users.noreply.github.com>

--- src/sparsematrix.jl
+++ src/sparsematrix.jl
@@ -49,10 +49,12 @@ SparseMatrixCSC(m, n, colptr::ReadOnly, rowval::ReadOnly, nzval::Vector) =
 
 """
     SparseMatrixCSC{Tv,Ti}(::UndefInitializer, m::Integer, n::Integer)
+    SparseMatrixCSC{Tv,Ti}(::UndefInitializer, (m,n)::NTuple{2,Integer})
 
 Creates an empty sparse matrix with element type `Tv` and integer type `Ti` of size `m × n`.
 """
 SparseMatrixCSC{Tv,Ti}(::UndefInitializer, m::Integer, n::Integer) where {Tv, Ti} = spzeros(Tv, Ti, m, n)
+SparseMatrixCSC{Tv,Ti}(::UndefInitializer, mn::NTuple{2,Integer}) where {Tv, Ti} = spzeros(Tv, Ti, mn...)
 
 """
     FixedSparseCSC{Tv,Ti<:Integer} <: AbstractSparseMatrixCSC{Tv,Ti}
