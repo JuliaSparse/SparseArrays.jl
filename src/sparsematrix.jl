f1f1a5c675bd5a3348da784bccd3d22f2857722f
termi-official <termi-official@users.noreply.github.com>
2024-07-05 11:54:05 +0200

    Again

--- src/sparsematrix.jl
+++ src/sparsematrix.jl
@@ -326,23 +326,8 @@ Return a vector of the column indices of `A`. Any modifications to the returned
 vector will mutate `A` as well. Providing access to how the row indices are
 stored internally can be useful in conjunction with iterating over structural
 nonzero values. See also [`nonzeros`](@ref) and [`nzrange`](@ref).
-
-# Examples
-```jldoctest
-julia> A = sparse(2I, 3, 3)
-3×3 SparseMatrixCSC{Int64, Int64} with 3 stored entries:
- 2  ⋅  ⋅
- ⋅  2  ⋅
- ⋅  ⋅  2
-
-julia> colvals(A)
-3-element Vector{Int64}:
- 1
- 2
- 3
-```
 """
-colvals(S::SorF)                                          = getfield(S, :colval)
+colvals(S::AbstractSparseMatrixCSR)                       = getfield(S, :colval)
 colvals(S::SparseMatrixCSRRowSubset)                      = colvals(S.parent)
 colvals(S::UpperTriangular{<:Any,<:SparseMatrixCSRUnion}) = colvals(S.data)
 colvals(S::LowerTriangular{<:Any,<:SparseMatrixCSRUnion}) = colvals(S.data)
