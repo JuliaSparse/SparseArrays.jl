2e78a15a2ada9c1a6472921a2fb0551d1d36f88f
Viral B. Shah <ViralBShah@users.noreply.github.com>
2026-09-09 06:30:41 -0400

    Trim redundant sparse test cases

--- test/umfpack.jl
+++ test/umfpack.jl
@@ -532,12 +532,10 @@ end
 end
 
 
-for Ti in Base.uniontypes(UMFPACK.UMFITypes)
-    A = I + sprandn(100, 100, 0.01)
-    Af = lu(A)
-    UMFPACK.umfpack_report_numeric(Af, 0)
-    UMFPACK.umfpack_report_symbolic(Af, 0)
-end
+A = I + sprandn(100, 100, 0.01)
+Af = lu(A)
+UMFPACK.umfpack_report_numeric(Af, 0)
+UMFPACK.umfpack_report_symbolic(Af, 0)
 
 end # Base.USE_GPL_LIBS
 
