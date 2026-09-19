fa5925a36ea0c0d19f1cd098a73368c26a5308b2
Viral B. Shah <virals@gmail.com>
2026-09-19 14:30:27 +0000

    Match mixed solve checks to CHOLMOD factor precision

--- test/cholmod_ops.jl
+++ test/cholmod_ops.jl
@@ -202,7 +202,7 @@ Random.seed!(123)
         d = fill(one(elty2), 5)
         @test A1Sparse*d ≈ A1*d
         @test A1Sparse'*d ≈ A1'*d
-        @test A1pd \ d ≈ Matrix(A1pd) \ d
+        @test A1pd \ d ≈ Matrix(A1pd) \ d rtol=sqrt(eps(Tv))
     end
 end
 
